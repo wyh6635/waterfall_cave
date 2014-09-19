2055a04cb1bd52c768bed675f814d452fa390f0a
wyh6635 <414661194@qq.com>
2014-09-19 21:53:55 +0800

    Revert "初始化花果山水帘洞项目"
    This reverts commit e98c96e9d4d0d365267e94a51d762ff9b59069c9.

--- src/main/java/com/iboxpay/hgm/wfc/cst/CsonConstants.java
+++
@@ -1,6 +0,0 @@
-package com.iboxpay.hgm.wfc.cst;
-
-public interface CsonConstants {
-
-	String CHARSET_UTF_8 = "UTF-8";
-}
--- src/main/webapp/index.jsp
+++
@@ -1,4 +0,0 @@
-<script language="javascript">
-	window.location.href="index/login.do";
-</script>
-
